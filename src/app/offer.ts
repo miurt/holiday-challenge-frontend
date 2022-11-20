e54b4961e7aa0b6287eb5105676a9441787650eb
miurt <mariia.iurtaeva@gmail.com>
2022-11-20 19:22:42 +0100

    Still dependencies fixes

--- src/app/offer.ts
+++ src/app/offer.ts
@@ -2,15 +2,9 @@ export interface Offer {
     hotelid: string;
     departuredate: Date;
     returndate: Date;
-    //countadults: number;
-    //countchildren: number;
     price: number;
-    //inbounddepartureairport: string;
-    //inboundarrivalairport: string;
     inboundairline: string;
     inboundarrivaldatetime: Date;
-    //outbounddepartureairport: string;
-    //outboundarrivalairport: string;
     outboundairline: string;
     outboundarrivaldatetime: Date;
     mealtype: string;
